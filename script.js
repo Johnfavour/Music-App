795aab873ef21f6458e2c2f4c4244d11ae07a53c
Johnfavour <favourjohn@gmail.com>
2023-07-09 21:12:25 +0100

    My portfolio project

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const music = new Audio('Tatiana.mp3');
+const music = new Audio('audio/Tatiana.mp3');
 //music.play();
 
 
@@ -11,10 +11,89 @@ const songs = [
     },
     {
         id: '2',
-        songName: `Alan walker-Fade<br>
+        songName: `On my Way<br>
         <div class="subtitle">Alan Walker</div>`,
         poster: "images/alan.jpg",
     },
+    {
+        id: '3',
+        songName:` Love Story<br>
+        <div class="subtitle">Taylor Swift</div>`,
+        poster:"images/taylor.jpg",
+    },
+    {
+        id: '4',
+        songName:` In the stars <br>
+        <div class="subtitle">Benson Boone</div>`,
+        poster:"images/benson.jpg ",
+    },
+    {
+        id: '5',
+        songName:` Scars <br>
+        <div class="subtitle">Keenan te</div>`,
+        poster:"images/keenan.jpg",
+    },
+    {
+        id: '6',
+        songName:` Anyone <br>
+        <div class="subtitle">Justin Bieber</div>`,
+        poster:"images/justin.jpg",
+    },
+    {
+        id: '7',
+        songName:` Believe Me <br>
+        <div class="subtitle">Johnny Drille</div>`,
+        poster:"images/johnny.jpg",
+    },
+    {
+        id: '8',
+        songName:` Bazaar <br>
+        <div class="subtitle">Nicki Minaj</div>`,
+        poster:"images/nicki.jpg",
+    },
+    {
+        id: '9',
+        songName:` Photograph <br>
+        <div class="subtitle">Ed Sheeran</div>`,
+        poster:"images/ed sheeran.jpg",
+    },
+    {
+        id: '10',
+        songName:` Easy on me <br>
+        <div class="subtitle">Adele</div>`,
+        poster:"images/adele.jpg",
+    },
+    {
+        id: '11',
+        songName:` Just look up <br>
+        <div class="subtitle">Ariana Grande</div>`,
+        poster:"images/ariana.jpg",
+    },
+    {
+        id: '12',
+        songName:` Lift me up <br>
+        <div class="subtitle">Rihanna</div>`,
+        poster:"images/Rihanna.jpg",
+    },
+    {
+        id: '13',
+        songName:` Birthday <br>
+        <div class="subtitle">Anne marie</div>`,
+        poster:"images/Anne.jpg",
+    },
+    {
+        id: '14',
+        songName:` Blank Space <br>
+        <div class="subtitle">Taylor Swift</div>`,
+        poster:"images/taylorswift.jpg",
+    },
+    {
+        id: '15',
+        songName:` Invasion of privacy <br>
+        <div class="subtitle">Cardi B</div>`,
+        poster:"images/cardi.webp",
+    },
+
     // Add more song objects here
 ];
 
@@ -53,6 +132,8 @@ const makeAllPlays = () =>{
 }
 
 let index = 0;
+let poster_master = document.getElementById('poster_master');
+let title = document.getElementById('title');
 
 Array.from(document.getElementsByClassName('playlists')).forEach((element)=>{
     element.addEventListener('click', (e)=>{
@@ -64,39 +145,6 @@ Array.from(document.getElementsByClassName('playlists')).forEach((element)=>{
         music.play();
     })
 })
-
-// create Array
-// const songs = [
-//     {
-//         id:'1',
-//         songName:`Helplessly<br>
-//         <div class="subtitle">Tatiana manaois</div>`,
-//         poster: "images/tatiana.jpg", //where i can change the image
-//     },
-//     {
-//         id:'2',
-//         songName:`Alan walker-Fade<br>
-//         <div class="subtitle">Alan Walker</div>`,
-//         poster: "images/alan.jpg",
-//     },
-//     //all the ids(from 3-15) should be added above
-// ]
-
-// Array.from(document.getElementsByClassName('songitem')).forEach((element, i)=>{
-//     element.getElementsByTagName('img')[0].src = songs[i].poster;
-//     element.getElementsByTagName('h5')[0].innerHTML = songs[i].songName;
-// })
-
-// let masterPlay = document.getElementById('masterPlay');
-
-// masterPlay.addEventListener('click',()=>{
-//     if (music.paused || music.currentTime >=0) {
-//         music.play();
-//     } else {
-//         music.pause();
-//     }
-// } )
-
 
 
 
